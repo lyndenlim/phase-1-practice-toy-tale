14658d1b4071df18b5c986710dc78ed795f3e93b
Lynden Lim <lyndenlim15@gmail.com>
2022-01-27 22:41:35 -0500

    commit tree 48f19bb28b102fddedb439ba5acad60da4b9daf0

--- src/index.js
+++ src/index.js
@@ -13,3 +13,81 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function fetchData() {
+  return fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(toyData => renderToys(toyData))
+}
+
+function renderToys(toyData) {
+  toyData.forEach(toy => {
+    const div = document.createElement("div")
+    div.classList.add("card")
+    const h2 = document.createElement("h2")
+    h2.textContent = toy.name
+    const img = document.createElement("img")
+    img.classList.add("toy-avatar")
+    img.src = toy.image
+    const p = document.createElement("p")
+    p.textContent = `${toy.likes} likes`
+    const button = document.createElement("button")
+    button.classList.add("like-btn")
+    button.id = toy.id
+    button.textContent = "Like❤️"
+    button.addEventListener("click", e => {
+      editToy(e)
+    })
+
+    div.appendChild(h2)
+    div.appendChild(img)
+    div.appendChild(p)
+    div.appendChild(button)
+    document.querySelector("#toy-collection").append(div)
+  })
+}
+
+function createNewToy() {
+  const form = document.querySelector(".add-toy-form")
+  form.addEventListener("submit", e => {
+    e.preventDefault()
+    const toyName = e.target[0].value
+    const toyImg = e.target[1].value
+    postNewToy(toyName, toyImg)
+  })
+}
+
+function postNewToy(toyName, toyImg) {
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "name": `${toyName}`,
+      "image": `${toyImg}`,
+      "likes": 0
+    })
+  })
+}
+
+function editToy(e) {
+  const toyId = e.target.id
+  let numberOfLikes = parseInt(e.target.parentNode.children[2].textContent)
+  numberOfLikes++
+  fetch(`http://localhost:3000/toys/${toyId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "likes": numberOfLikes
+    })
+  })
+}
+
+
+fetchData()
+createNewToy()
